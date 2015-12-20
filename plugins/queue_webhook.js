cfc175fdb1a7ce294ad248906c1d180e4cea5c92
Etienne Garnier <garnier.etienne@gmail.com>
2015-12-20 20:03:27 +0100

    include the SMTP envelope into the POST request to the webhook URL as headers

--- plugins/queue_webhook.js
+++ plugins/queue_webhook.js
@@ -11,15 +11,20 @@ var fs = require("fs");
 // correctly. As a workaround we store the message in a temporary file to be
 // able to get the byte lenth of the message before transferring it over HTTP.
 //
-//  The callback function return two arguments:
+//  The callback function return three arguments:
 //  * err: any error that occur during the reception of the message
 //  * messageFilePath: the path to the temporary file where the message is
 //    stored
+//  * SMTPEnvelope: The SMTP envelope (MAIL FROM and RCPT TO)
 var receiveMessage = function(connection, callback) {
   var tmp = os.tmpdir();
   var messageStream = connection.transaction.message_stream;
   var messageFilePath = tmp + "/" + connection.transaction.uuid;
   var messageFile = fs.createWriteStream(messageFilePath);
+  var SMTPEnvelope = {
+    "MAIL-FROM": connection.transaction.mail_from,
+    "RCPT-TO": connection.transaction.rcpt_to
+  }
 
   messageStream.once("error", function(err) {
     return callback("(message stream) " + err);
@@ -30,7 +35,7 @@ var receiveMessage = function(connection, callback) {
   });
 
   messageFile.once("close", function () {
-      return callback(null, messageFilePath);
+      return callback(null, messageFilePath, SMTPEnvelope);
   });
 
   messageStream.pipe(messageFile);
@@ -40,65 +45,72 @@ var receiveMessage = function(connection, callback) {
 //
 // Submit the message in a non chunked way to the destination URL.
 // The message is POSTed in plain text.
-var deliverMessage = function(messageFilePath, webhookURL, callback) {
-  var httpEndpoint = url.parse(webhookURL);
-  var messageLength = fs.statSync(messageFilePath)["size"];
-  var messageFile = fs.createReadStream(messageFilePath);
+var deliverMessage =
+  function(messageFilePath, SMTPEnvelope, webhookURL, callback) {
+    var httpEndpoint = url.parse(webhookURL);
+    var messageLength = fs.statSync(messageFilePath)["size"];
+    var messageFile = fs.createReadStream(messageFilePath);
 
-  var request = http.request({
-    protocol: httpEndpoint.protocol,
-    host: httpEndpoint.hostname,
-    port: httpEndpoint.port,
-    path: httpEndpoint.path,
-    method: "POST",
-    headers: {
-      "Content-Type": "text/plain",
-      "Content-Length": messageLength
-    }
-  });
-
-  messageFile.once("error", function(err) {
-    return callback("(message file) " + err);
-  });
-
-  request.once("error", function(err) {
-    return callback("(request) " + err)
-  });
-
-  request.once("response", function(response) {
-    var text = "";
-
-    response.on("data", function(chunk) {
-      text += chunk;
+    var request = http.request({
+      protocol: httpEndpoint.protocol,
+      host: httpEndpoint.hostname,
+      port: httpEndpoint.port,
+      path: httpEndpoint.path,
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+        "Content-Length": messageLength,
+        "SMTP-MAIL-FROM": SMTPEnvelope["MAIL-FROM"],
+        "SMTP-RCPT-TO": SMTPEnvelope["RCPT-TO"]
+      }
     });
 
-    response.on("end", function() {
-      var result = {success: response.statusCode == 200, text: text};
-      return callback(null, result);
+    messageFile.once("error", function(err) {
+      return callback("(message file) " + err);
     });
-  });
 
-  messageFile.pipe(request);
-}
+    request.once("error", function(err) {
+      return callback("(request) " + err)
+    });
+
+    request.once("response", function(response) {
+      var text = "";
+
+      response.on("data", function(chunk) {
+        text += chunk;
+      });
+
+      response.on("end", function() {
+        var result = {success: response.statusCode == 200, text: text};
+        return callback(null, result);
+      });
+    });
+
+    messageFile.pipe(request);
+  }
 
 exports.hook_queue = function(next, connection) {
   var webhookURL = this.config.get("webhook_url") || "http://localhost";
 
-  receiveMessage(connection, function(err, messageFilePath) {
+  receiveMessage(connection, function(err, messageFilePath, SMTPEnvelope) {
 
     if (err) {
       connection.logerror(err);
       return next(DENYSOFT, "An error prevented the reception of the message");
     }
 
-    deliverMessage(messageFilePath, webhookURL, function(err, result) {
+    deliverMessage(
+      messageFilePath, SMTPEnvelope, webhookURL, function(err, result) {
 
-      if (err) {
-        connection.logerror(err);
-        return next(DENYSOFT, "An error prevented the delivery of the message");
+        if (err) {
+          connection.logerror(err);
+          return next(
+            DENYSOFT, "An error prevented the delivery of the message"
+          );
+        }
+
+        return next((result.success) ? OK : DENY, result.text);
       }
-
-      return next((result.success) ? OK : DENY, result.text);
-    });
+    );
   });
 };
